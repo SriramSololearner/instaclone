46fb2117a22129d025e13b0679538a1c687c3ecb
user name <user email>
2022-05-29 19:12:51 +0530

    instaclone

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card.js";
+import Navbar from "./Navbar.js";
+
+const LandingPage = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fun() {
+      let data1 = await fetch("https://instaclone-reactapp.herokuapp.com/posts");
+      let data = await data1.json();
+      console.log(data.user)
+      setUsers([...data.user]);
+    }              
+
+    fun();
+  }, []);
+
+  return (
+    <div className="site-container">
+      <Navbar />
+      <div className="container">
+        {users.length
+          ? users.map((obj, idx) => {
+              return <Card key={"vamsi" + idx} obj={obj} />;
+            })
+          : ""}
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
